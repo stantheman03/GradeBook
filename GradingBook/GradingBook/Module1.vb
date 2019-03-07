2f301253b6e39fa3ff8051e513322bdd677b1ed1
Stanley Philip <Stanley Philip>
2019-03-06 22:46:10 -0500

    first

---
+++ GradingBook/GradingBook/Module1.vb
@@ -0,0 +1,67 @@
+﻿Module Module1
+
+    Class student
+        Public firstname As String
+        Public lastname As String
+        Public grade As Integer
+        Public passsOrfail As String
+    End Class
+
+    Dim students(50) As student
+    Dim studentcount As Integer = 0
+
+    Sub Main()
+        For i = 0 To 50
+            students(i) = New student
+        Next
+
+        Dim selection As Char
+
+        While selection <> "c"
+
+            Console.WriteLine("welcome to the student gradebook")
+        Console.WriteLine("number of students" & studentcount)
+        Console.WriteLine("a. add student")
+        Console.WriteLine("b. view student")
+        Console.WriteLine("c. quit program")
+
+            selection = Console.ReadLine
+
+
+            If selection = "a" Then
+                Console.WriteLine("enter firstname ")
+                students(studentcount).firstname = Console.ReadLine
+                Console.WriteLine("enter lastname ")
+                students(studentcount).lastname = Console.ReadLine
+                Console.WriteLine("enter grade ")
+                students(studentcount).grade = Console.ReadLine
+                Console.WriteLine("enter pass or fail ")
+                students(studentcount).passsOrfail = Console.ReadLine
+
+                studentcount += 1
+
+                Console.WriteLine("students entered !! ")
+                Console.ReadLine()
+
+            ElseIf selection = "b" Then
+
+                For i = 0 To studentcount - 1
+                    Console.WriteLine("student firstname: " & students(i).firstname)
+                    Console.WriteLine("student lastname: " & students(i).lastname)
+                    Console.WriteLine("student grade: " & students(i).grade)
+                    Console.WriteLine("student passorfail: " & students(i).passsOrfail)
+                    Console.ReadLine()
+
+                Next
+
+            ElseIf selection = "c" Then
+
+
+
+            End If
+        End While
+
+
+    End Sub
+
+End Module
---
+++ GradingBook/GradingBook/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("GradingBook")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("GradingBook")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("2c8a7733-1b97-40b2-b46e-d48f877d5f4e")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
